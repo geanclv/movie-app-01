3ccb572359b0f8ebadf4adc145649c8f7ba54aa4
Geancarlo Leiva Via <geanclv@gmail.com>
2022-11-13 18:04:03 -0500

    Creando el árbol de rutas (Routes)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
-const routes: Routes = [];
+// Defining the routes in the app
+const routes: Routes = [
+  {
+    path: "", //root
+    component: HomeComponent //name of the class in *.component.ts
+  },
+  {
+    path: "movies",
+    component: MoviesComponent
+  },
+  {
+    path: "**", //used as comodin, in this case for 404 cases
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
